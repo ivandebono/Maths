baf4a3ebd742bd07998c69496c09db1216e48fee
Ivan Debono <ivan.debono@in2p3.fr>
2017-04-23 18:41:18 +0200

    Update CopelandErdos.py

--- CopelandErdos.py
+++ CopelandErdos.py
@@ -5,26 +5,26 @@ ivandebono [at] idebono [dot] eu
 
 23 April 2017
 
-PURPOSE Output the Copeland-Erdős constant up to desired digit
+PURPOSE Output the Copeland-Erdős constant up to desired nth prime
         (See http://mathworld.wolfram.com/Copeland-ErdosConstant.html)
 
-INPUT   last: last prime (e.g. last=10 means up to 10th prime)
+INPUT   n: position of last prime (e.g. last=10 means up to 10th prime)
 
 OUTPUT  copeland_erdos_str: string representation of Copeland-Erdős constant
         copeland_erdos: Copeland-Erdős constant, in Python Decimal format
         (i.e. floating point with any user-defined precision),
-        to same number of digits as last digit
+        to same number of primes as n
 """    
 
 from decimal import getcontext, Decimal 
 import numpy as np
 import sympy
 
-def CopelandErdos(last): 
+def CopelandErdos(n): 
 
-    # First generate list of primes up to last
+    # First generate list of primes up to n
     primes=[]
-    for prime in np.arange(1,last+1): primes.append(sympy.ntheory.generate.prime(prime))
+    for prime in np.arange(1,n+1): primes.append(sympy.ntheory.generate.prime(prime))
 
     copeland_erdos_str='0.'+''.join(str(p) for p in primes)    
     getcontext().prec = len(copeland_erdos_str)
